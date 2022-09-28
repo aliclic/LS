b41806e5b169979d4809e80ecedb30f91867bf2e
alicvictor <alicvictor11@gmail.com>
2022-09-28 19:33:41 -0300

    imc ok

--- exercicios/web apps/bmi-css/main.js
+++ exercicios/web apps/bmi-css/main.js
@@ -3,6 +3,22 @@ const imcStatus = {
     label: 'Peso Normal',
     background: 'border border-sucess bg-success text-white',
   },
+  underWeight: {
+    label: 'Abaixo do Peso',
+    background: 'border border-warning bg-warning text-white',
+  },
+  stritclyOverWeight: {
+    label: 'Marginalmente Acima do Peso',
+    background: 'border border-warning bg-warning text-white',
+  },
+  overWeight: {
+    label: 'Acima do Peso Ideal',
+    background: 'border border-warning bg-warning text-white',
+  },
+  obese: {
+    label: 'Obeso',
+    background: 'border border-danger bg-danger text-white'
+  }
 };
 
 function handleSubmit(event) {
@@ -20,5 +36,32 @@ function handleSubmit(event) {
 }
 
 function imc({ height, weight, sex }) {
-  return 'normalWeight';
+  const imc = weight / (height ** 2);
+
+  if (sex == 'female') {
+    if (imc < 19.1) {
+      return 'underWeight';
+    } else if (imc < 25.8) {
+      return 'normalWeight';
+    } else if (imc < 27.3) {
+      return 'stritclyOverWeight';
+    } else if (imc < 32.3) {
+      return 'overWeight';
+    } 
+      return 'obese';
+  }
+
+  if (sex == 'male') {
+    if (imc < 20.7) {
+      return 'underWeight';
+    } else if (imc < 26.4) {
+      return 'normalWeight';
+    } else if (imc < 27.8) {
+      return 'stritclyOverWeight';
+    } else if (imc < 31.1) {
+      return 'overWeight';
+    } 
+      return 'obese';
+  }
+
 }
